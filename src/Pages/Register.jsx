762d6a659c3a560c8181cb2f79b4261409af93ec
mohotasir <mohotasir1036@gmail.com>
2024-08-27 14:35:57 +0600

    add:mobile input field

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import useAxiosSequre from "../Hooks/axiosSecqure/useAxiosSequre";
-
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css';
 export default function Register() {
   const [error, setError] = useState("");
+  const [showPass, setShowPass] = useState(false);
   const [success, setSuccess] = useState("");
   const axiosSequre = useAxiosSequre();
+  const [selectedAddress, setSelectedAddress] = useState("yes");
+  const [phone, setPhone] = useState("");
 
+  const handleOnChange = (value) => {
+    setPhone(value);
+  };
+  const handleSelectChange = (event) => {
+    setSelectedAddress(event.target.value);
+  };
   const handleForm = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -87,35 +99,43 @@ export default function Register() {
           >
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Last  Name.."
               />
             </div>
-            <div className="">
-            <input
-            className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
-            type="text"
-            name="phone"
-            placeholder="Full  Name.."
-          />       
+      <div>
+      <div className="phone-input w-full bg-gray-700 py-1">
+                <PhoneInput
+                    country={'bd'}
+                    onChange={handleOnChange}
+                    placeholder="Enter your mobile number" // Placeholder text
+                    inputProps={{
+                        name: "phone",
+                        required: true,
+                        autoFocus: true,
+                    }}
+                    containerClass="w-full" // Full width for the container
+                   
+                />
             </div>
+    </div>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="User Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="date"
                 name="phone"
                 placeholder="Date of Birth.."
@@ -123,21 +143,21 @@ export default function Register() {
             </div>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Father's  Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Mother's  Name.."
               />
             </div>
             <div>
-            <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+              <input
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Enter NID / Passport number.."
@@ -145,20 +165,22 @@ export default function Register() {
             </div>
             {/* border divider */}
             <div className=" flex justify-between my-8">
-                 <div className="border-b w-full border-gray-600"></div>
-                 <div className="w-full text-center text-gray-500 text-xs font-semibold">PRESENT ADDRESS</div>
-                 <div className="border-b border-gray-600 w-full"></div>
+              <div className="border-b w-full border-gray-600"></div>
+              <div className="w-full text-center text-gray-500 text-xs font-semibold">
+                PRESENT ADDRESS
+              </div>
+              <div className="border-b border-gray-600 w-full"></div>
             </div>
             {/* present address inputs */}
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Last  Name.."
@@ -166,13 +188,13 @@ export default function Register() {
             </div>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Last  Name.."
@@ -180,29 +202,43 @@ export default function Register() {
             </div>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
-             
             </div>
             {/* border divider */}
             <div className=" flex justify-between my-8">
-                 <div className="border-b w-full border-gray-600"></div>
-                 <div className="w-full text-center text-gray-500 text-xs font-semibold">PARMANENT ADDRESS</div>
-                 <div className="border-b border-gray-600 w-full"></div>
+              <div className="border-b w-full border-gray-600"></div>
+              <div className="w-full text-center text-gray-500 text-xs font-semibold">
+                PARMANENT ADDRESS
+              </div>
+              <div className="border-b border-gray-600 w-full"></div>
             </div>
             {/* inputs of parmanent address */}
+            <div className="flex gap-3 items-center">
+              <span className="text-gray-400 font-semibold">Same to Present Address : </span>
+              <select
+                className=" px-12 border-gray-700 bg-gray-700 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
+                name="selectAddress"
+                onChange={handleSelectChange}
+                value={selectedAddress}
+              >
+                <option value="yes">YES </option>
+                <option value="no">NO </option>
+              </select>
+            </div>
+            <div className={`${selectedAddress === 'yes'  ? 'hidden' : 'flex'} flex-col gap-3`}>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Last  Name.."
@@ -210,13 +246,13 @@ export default function Register() {
             </div>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="Last  Name.."
@@ -224,37 +260,49 @@ export default function Register() {
             </div>
             <div className="flex gap-3 flex-col md:flex-row">
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
                 type="text"
                 name="phone"
                 placeholder="First  Name.."
               />
-             
+            </div>
             </div>
             {/* border divider */}
             <div className=" flex justify-between my-8">
-                 <div className="border-b w-full border-gray-600"></div>
-                 <div className="w-full text-center text-gray-500 text-xs font-semibold">Academic Qualification</div>
-                 <div className="border-b border-gray-600 w-full"></div>
+              <div className="border-b w-full border-gray-600"></div>
+              <div className="w-full text-center text-gray-500 text-xs font-semibold">
+                Academic Qualification
+              </div>
+              <div className="border-b border-gray-600 w-full"></div>
             </div>
             {/* border divider */}
             <div className=" flex justify-between my-8">
-                 <div className="border-b w-full border-gray-600"></div>
-                 <div className="w-full text-center text-gray-500 text-xs font-semibold">Password</div>
-                 <div className="border-b border-gray-600 w-full"></div>
+              <div className="border-b w-full border-gray-600"></div>
+              <div className="w-full text-center text-gray-500 text-xs font-semibold">
+                Password
+              </div>
+              <div className="border-b border-gray-600 w-full"></div>
             </div>
             {/* password field */}
             <div className="flex gap-3 flex-col ">
+              <div className="relative">
+                <input
+                  className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
+                  type={showPass ? "text" : "password"}
+                  name="password"
+                  placeholder="Enter Password .."
+                />
+                <div
+                  onClick={() => setShowPass(!showPass)}
+                  className="absolute right-5 top-3 text-xl text-gray-400"
+                >
+                  {showPass ? <FaRegEye /> : <FaRegEyeSlash />}
+                </div>
+              </div>
               <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
-                type="text"
-                name="phone"
-                placeholder="Enter Password .."
-              />
-              <input
-                className="border w-full border-gray-700 bg-gray-700 p-3 rounded focus:border-gray-600 text-gray-200 outline-none"
-                type="text"
-                name="phone"
+                className="border w-full border-gray-700 bg-gray-700 px-3 py-2 rounded focus:border-gray-600 text-gray-200 outline-none"
+                type="password"
+                name="conPassword"
                 placeholder="Confirm Password .."
               />
             </div>
